a492cf7285af6e47ab325ac87b506a67e3fda4cf
konstantin.khodykin <konstantin.khodykin@03d5a937-c853-0410-890f-3b83e6fed59e>
2010-11-09 23:31:05 +0000

    Added GetArray and GetDictionary methods

--- branches/Listelli/Shop/Helpers/FormCollectionExtender.cs
+++ branches/Listelli/Shop/Helpers/FormCollectionExtender.cs
@@ -37,6 +37,45 @@ namespace Dev.Helpers
             }
             return result;
         }
+
+
+        /// <summary>
+        /// Designed to get checked checkboxes
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="form"></param>
+        /// <param name="prefix"></param>
+        /// <returns></returns>
+        public static T[] GetArray<T>(this FormCollection form, string prefix)
+        {
+            string[] items = form.AllKeys.Where(k => k.StartsWith(prefix)).ToArray();
+            List<T> res = new List<T>();
+            foreach (string key in items)
+            {
+                if (form[key] == "true,false")
+                    res.Add((T)Convert.ChangeType(key.Replace(prefix, string.Empty), typeof(T)));
+            }
+            T[] result = res.ToArray();
+            return result;
+        }
+
+        public static Dictionary<TKey, TValue> GetDictionary<TKey, TValue>(this FormCollection form, string prefix)
+        {
+            string[] items = form.AllKeys.Where(k => k.StartsWith(prefix)).ToArray();
+            Dictionary<TKey, TValue> result = new Dictionary<TKey, TValue>();
+            foreach (var key in items)
+            {
+                TKey dicKey = (TKey)Convert.ChangeType(key.Replace(prefix, string.Empty), typeof(TKey));
+                TValue value = default(TValue);
+                if (typeof(TValue) == typeof(bool))
+                    value = (TValue)(object)(form[key] == "true,false");
+                else
+                    value = (TValue)Convert.ChangeType(form[key], typeof(TValue));
+                result.Add(dicKey, value);
+            }
+            return result;
+        }
     }
+
 
 }

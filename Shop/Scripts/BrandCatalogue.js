961bc0c8e2db4d0fc5f41b36d97cb31e2ce42555
konstantin.khodykin <konstantin.khodykin@03d5a937-c853-0410-890f-3b83e6fed59e>
2010-11-10 18:30:04 +0000

    Added image width update Added jquery.combobox.js

---
+++ Shop/Scripts/BrandCatalogue.js
@@ -0,0 +1,43 @@
+﻿/// <reference path="jquery.js"/>
+/// <reference path="jquery.combobox.js"/>
+
+/*
+
+comboboxContainerClass
+    the CSS class name for the entire control
+comboboxValueContainerClass
+    the CSS class name for the area that contains the selected value and the drop-down button
+comboboxValueContentClass
+    the CSS class name for the selected value displayed
+comboboxDropDownClass
+    the CSS class name for the drop down list container
+comboboxDropDownButtonClass
+    the CSS class name for the drop down button 
+comboboxDropDownItemClass
+    the CSS class name for an item in the drop down list 
+comboboxDropDownItemHoverClass
+    the CSS class name for an item when the mouse is over
+comboboxDropDownGroupItemHeaderClass
+    the CSS class name for the group header item in the drop down list 
+comboboxDropDownGroupItemContainerClass
+    the CSS class name for the group section in the drop down list 
+animationType
+    the CSS class name for the type of animation for the drop down list
+width
+    the width of the combobox control
+height
+    the maximum height of the drop-down list. 
+
+
+*/
+
+var BrandCatalogue = {
+    initialize: function () {
+        $(function () {
+            try {
+                $("select#id").combobox({ comboboxContainerClass: "class1", comboboxValueContainerClass: "class2", comboboxDropDownButtonClass:"class3" });
+            }
+            catch (e) { /*whatever*/}
+        });
+    }
+}
--- Shop/Views/Products/Product.ascx
+++ Shop/Views/Products/Product.ascx
@@ -15,7 +15,6 @@
 %>
 <div class="productItem">
     <div class="img">
-    <p class="pid">
         <% 
             Shop.Models.ProductImage img = Model.ProductImages.Where(pi => pi.Default).FirstOrDefault();
             if (img != null)
@@ -30,7 +29,6 @@
                 Response.Write(productClickLink.Replace("[IMAGE]", Model.Name));
             }
         %>
-        </p>
         <%if (Model.IsNew)
           { %>
         <div class="newItem">
@@ -45,9 +43,5 @@
                 <% Html.RenderPartial("ProductStaticAttributes", Model.ProductAttributeStaticValues); %>
             </div>
         </div>
-
-        <%--<button class="getp">d width</button>
-            <div class="showMe" style="width: 140px; height: 20px;"></div>--%>
-            
 </div>
 <%} %>

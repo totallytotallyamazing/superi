a0eef92f9a3a4eb5558f1941434bb6fb2228b8b1
konstantin.khodykin <konstantin.khodykin@03d5a937-c853-0410-890f-3b83e6fed59e>
2010-11-18 17:34:03 +0000

    Dynamic brands loading inmplemented in BrandCatalog

--- branches/Listelli/Shop/Scripts/BrandCatalogue.js
+++ branches/Listelli/Shop/Scripts/BrandCatalogue.js
@@ -1,51 +1,30 @@
 ﻿/// <reference path="jquery.js"/>
 /// <reference path="jquery.combobox.js"/>
 
-/*
-
-comboboxContainerClass
-the CSS class name for the entire control
-comboboxValueContainerClass
-the CSS class name for the area that contains the selected value and the drop-down button
-comboboxValueContentClass
-the CSS class name for the selected value displayed
-comboboxDropDownClass
-the CSS class name for the drop down list container
-comboboxDropDownButtonClass
-the CSS class name for the drop down button 
-comboboxDropDownItemClass
-the CSS class name for an item in the drop down list 
-comboboxDropDownItemHoverClass
-the CSS class name for an item when the mouse is over
-comboboxDropDownGroupItemHeaderClass
-the CSS class name for the group header item in the drop down list 
-comboboxDropDownGroupItemContainerClass
-the CSS class name for the group section in the drop down list 
-animationType
-the CSS class name for the type of animation for the drop down list
-width
-the width of the combobox control
-height
-the maximum height of the drop-down list. 
-
-
-*/
-
 var BrandCatalogue = {
     initialize: function () {
         $(function () {
-                $('#selectBrand').combobox({
-                    comboboxContainerClass: "cbBrandContainer",
-                    comboboxValueContainerClass: "cbBrandValueContainer",
-                    comboboxDropDownButtonClass: "cbBrandButton",
-                    comboboxDropDownClass: "cbBrandDropDown",                    
-                    comboboxValueContentClass: "cbBrandValueContent",
-                    animationType: "fade",
-                    width: "153px"
-                });
+            $('#selectBrand').combobox({
+                comboboxContainerClass: "cbBrandContainer",
+                comboboxValueContainerClass: "cbBrandValueContainer",
+                comboboxDropDownButtonClass: "cbBrandButton",
+                comboboxDropDownClass: "cbBrandDropDown",
+                comboboxValueContentClass: "cbBrandValueContent",
+                animationType: "fade",
+                width: "153px"
+            })
+            .change(function () {
+                BrandCatalogue.fetchBrands();
+            });
+            BrandCatalogue.fetchBrands();
         });
-        
+    },
+
+    fetchBrands: function () {
+        var element =  document.getElementById("selectBrand")
+        var id = element.options[element.selectedIndex].value; ;
+        $.get("/BrandCatalog/Brands/BrandLinks/" + id, function (data) { $("#topMenu").html(data); });
     }
-}
+};
 
 

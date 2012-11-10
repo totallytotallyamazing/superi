2b56fba397d0db7cda7b202b3be63955b28f36ee
konstantin.khodykin@gmail.com <konstantin.khodykin@gmail.com@03d5a937-c853-0410-890f-3b83e6fed59e>
2012-11-10 19:21:00 +0000

    a

---
+++ trunk/Proxy/Models/IPublicService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrsmProxy.Models
+{
+    [ServiceContract(Namespace = "http://loyalty.eps.lt/2012/08/public")]
+    public interface IPublicService
+    {
+        [OperationContract]
+        Profile GetProfile(string pan);
+
+        [OperationContract]
+        Profile GetProfileByCustomProperty(string propertyId, string value);
+
+        [OperationContract]
+        Profile GetProfileByDefaultProperty(string value);
+    }
+}

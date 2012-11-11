11fde8f4dc48c0c034e3bda09f2c2efa7b969d1a
konstantin.khodykin@gmail.com <konstantin.khodykin@gmail.com@03d5a937-c853-0410-890f-3b83e6fed59e>
2012-11-11 16:35:25 +0000

    added security module

---
+++ trunk/Proxy/App_Start/SecurityModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace BrsmProxy.App_Start
+{
+    public class SecurityModule :IHttpModule
+    {
+
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += BeginRequest;
+        }
+
+        void BeginRequest(object sender, EventArgs e)
+        {
+            string allowedIPs = WebConfigurationManager.AppSettings["AllowerIPs"];
+            bool permit = true;
+            if(allowedIPs != "*")
+            {
+                permit = allowedIPs.Split(';').Contains(HttpContext.Current.Request.UserHostAddress);
+            }
+            if (!permit)
+            {
+                throw new HttpException(403, "Access denied");
+            }
+        }
+    }
+}
